c7ce861fc66d6b24656d1642a5113a118589aae1
thiagoverardo <38051267+thiagoverardo@users.noreply.github.com>
2018-10-04 11:46:21 -0300

    Aps4.4

--- assets/lib/test.js
+++ assets/lib/test.js
@@ -2,19 +2,33 @@ document.addEventListener('DOMContentLoaded', function() {
     /*
         Inicializa uma medição de tempo, que será gravada no
         banco de dados cujo endereço é passado como parâmetro.
+        Altere o endereço abaixo para seu próprio banco de dados.
     */
     let ab = start('https://teste-93bc2.firebaseio.com/')
 
     /*
         Seleciona o elemento que, quando clicado, finaliza a
         medição de tempo e envia o resultado para o banco.
+        Altere o seletor abaixo para o do elemento desejado.
     */
     let element = document.querySelector('.link')
 
+    /*
+        A princípio, o restante do código não precisa mudar.
+    */
+
     element.addEventListener('click', function() {
-        /*
-            Finaliza medição e envia resultado para banco.
-        */
         ab.finish()
     })
+
+    let as = document.querySelectorAll('a')
+
+    for(let a of as) {
+        a.addEventListener('click', function(event) {
+            event.preventDefault()
+            setTimeout(function() {
+                window.location.assign(a.href)
+            }, 1000)
+        })
+    }
 })
